b507a880c8c3504054d7c4a2902ac3715ef4f978
kpdecker <kpdecker@gmail.com>
2010-04-18 10:07:07 -0500

    Simple testing framework

--- tipsAndTricks/app/assistants/first-assistant.js
+++ tipsAndTricks/app/assistants/first-assistant.js
@@ -1,6 +1,34 @@
 function FirstAssistant() {
+    this.tests = [
+    ];
 }
 
 FirstAssistant.prototype.setup = function() {
+    this.infoEl = this.controller.sceneElement.querySelector(".test-status");
+    this.failureLog = this.controller.sceneElement.querySelector(".failures");
+
+    var len = this.tests.length;
+    for (var i = 0; i < len; i++) {
+        this.runTest(i);
+    }
+    this.infoEl.innerHTML = $L("Tests complete");
 };
 
+FirstAssistant.prototype.runTest = function(i) {
+    this.infoEl.innerHTML = $L("Running test #{num}").interpolate({ num: i });
+    this.tests[i].exec(this);
+};
+
+FirstAssistant.prototype.failure = function(message) {
+    var li = this.controller.document.createElement("li");
+    li.innerHTML = message;
+    this.failureLog.appendChild(li);
+
+    Mojo.Log.error(message);
+
+    try {
+        throw new Error("Tracing");
+    } catch (err) {
+        DebugLogging.logError(err, "|    stack: ");
+    }
+};
