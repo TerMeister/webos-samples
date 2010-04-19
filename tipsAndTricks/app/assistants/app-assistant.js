eda2cf038268e8e190521fae6b4d9a2c4874c64c
kpdecker <kpdecker@gmail.com>
2010-04-18 20:52:24 -0500

    Convert the test app into a multistage app

---
+++ tipsAndTricks/app/assistants/app-assistant.js
@@ -0,0 +1,13 @@
+/* Copyright 2009 Palm, Inc.  All rights reserved. */
+function AppAssistant() {}
+
+AppAssistant.prototype.handleLaunch = function(params){
+    this.controller.createStageWithCallback({
+            name: "main",
+            lightweight: true,
+        },
+        function(stageController) {
+            stageController.pushScene("first");
+        },
+        Mojo.Controller.StageType.card);
+};
--- tipsAndTricks/app/assistants/stage-assistant.js
+++
@@ -1,5 +0,0 @@
-function StageAssistant() {}
-
-StageAssistant.prototype.setup = function() {
-    this.controller.pushScene("first");
-};
